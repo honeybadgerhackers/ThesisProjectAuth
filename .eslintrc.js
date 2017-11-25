cd4e0657ba3c9ec48188af58edceb2c51f03eb02
Rob Aird <robkaird@gmail.com>
2017-11-25 15:50:10 -0600

    (chore) adding files before changing gitattributes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,8 @@
 module.exports = {
-    "extends": "airbnb",
+    "parser": "babel-eslint",
+    "extends": ["airbnb", "exponent"],
     "rules": {
         "linebreak-style": 0,
-        // `.jsx` extension cannot be used with React Native
-        // https://github.com/airbnb/javascript/issues/982
         "react/jsx-filename-extension": ["error", { "extensions": [".js", ".jsx"] }]
     },
 };
--- App.js
+++ App.js
@@ -1,9 +1,13 @@
+import { Button, StyleSheet, Text, View, Alert } from 'react-native';
 import React from 'react';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
 import PropTypes from 'prop-types';
-import Expo from 'expo';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Facebook } from 'expo';
+import { StackNavigator } from 'react-navigation';
 
-const fbAppId = 530424093970397;
+const fbAppId = '530424093970397';
 
 const styles = StyleSheet.create({
   container: {
@@ -28,6 +32,8 @@ const styles = StyleSheet.create({
   },
 });
 
+// const sagaMiddleware = createSagaMiddleware();
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -35,34 +41,73 @@ export default class App extends React.Component {
       user: null,
       something: 'Goodbye!',
     };
-    this.something = this.something.bind(this);
   }
+  _handleFacebookLogin = async () => {
+    try {
+      const { type, token } = await Facebook.logInWithReadPermissionsAsync(
+        fbAppId, // Replace with your own app id in standalone app
+        { permissions: ['public_profile'] }
+      );
 
-  something() {
-    console.log(this.state.something);
+      switch (type) {
+        case 'success': {
+          // Get the user's name using Facebook's Graph API
+          const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
+          const profile = await response.json();
+          Alert.alert(
+            'Logged in!',
+            `Hi ${profile.name}!`,
+          );
+          break;
+        }
+        case 'cancel': {
+          Alert.alert(
+            'Cancelled!',
+            'Login was cancelled!',
+          );
+          break;
+        }
+        default: {
+          Alert.alert(
+            'Oops!',
+            'Login failed!',
+          );
+        }
+      }
+    } catch (e) {
+      Alert.alert(
+        'Oops!',
+        'Login failed!',
+      );
+    }
+  };
+
+  something = () => {
     this.setState({
       something: this.state.something === 'Goodbye!' ? 'Hello!' : 'Goodbye!',
     });
   }
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={[styles.box, styles.box1]}>
-          <Text>Open up App.js to start working on your app!</Text>
-        </View>
-        <View style={[styles.box, styles.box2]}>
-          <Text>Should say something: {this.state.something}</Text>
-          <Button
-            onPress={this.something}
-            title="Button"
-          />
-        </View>
-        <View style={[styles.box, styles.box3]}>
-          <Text style={{ fontSize: 30 }}>STOP TELLING ME WHAT TO DO</Text>
-        </View>
+  render = () => (
+    <View style={styles.container}>
+      <View style={[styles.box, styles.box1]}>
+        <Text>Open up App.js to start working on your app!</Text>
       </View>
-    );
-  }
+      <View style={[styles.box, styles.box2]}>
+        <Text>Should say something: {this.state.something}</Text>
+        <Button
+          title="Button"
+          onPress={this.something}
+        />
+        <Button
+          title="Login with Facebook"
+          onPress={this._handleFacebookLogin}
+        />
+      </View>
+      <View style={[styles.box, styles.box3]}>
+        <Text style={{ fontSize: 30 }}>STOP TELLING ME WHAT TO DO</Text>
+      </View>
+    </View>
+  );
 }
 
 App.propTypes = {
