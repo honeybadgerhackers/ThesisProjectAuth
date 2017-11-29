14f1138290e2b5d045285f43d3fb397964ae63e5
Rob Aird <robkaird@gmail.com>
2017-11-29 14:09:25 -0600

    (feat) displays polyline on the map, default Op Spark to Creole Creamery :)

--- src/components/screens/location.js
+++ src/components/screens/location.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Platform, Text, View, StyleSheet, Button } from 'react-native';
 import { MapView, Constants, Location, Permissions } from 'expo';
+import Polyline from '@mapbox/polyline';
 
 export default class WayPoint extends Component {
   state = {
@@ -8,12 +9,7 @@ export default class WayPoint extends Component {
     errorMessage: null,
     disableButton: false,
     wayPoints: [],
-    // region: {
-    //   latitude: 41.850033,
-    //   longitude: -87.6500523,
-    //   latitudeDelta: 0,
-    //   longitudeDelta: 0,
-    // },
+    coords: [],
   };
 
   componentWillMount() {
@@ -26,6 +22,28 @@ export default class WayPoint extends Component {
     }
   }
 
+  componentDidMount() {
+    this._getDirections('OperationSpark,NewOrleans,LA', 'CreoleCreamery,NewOrleans,LA');
+  }
+
+  _getDirections = async (startLoc, destinationLoc) => {
+    try {
+      let resp = await fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${startLoc}&destination=${destinationLoc}`);
+      let respJson = await resp.json();
+      let points = Polyline.decode(respJson.routes[0].overview_polyline.points);
+      let coords = points.map((point, index) => {
+        return {
+          latitude: point[0],
+          longitude: point[1]
+        }
+      });
+      this.setState({ coords });
+      console.log(this.state.coords);
+      return coords;
+    } catch (error) {
+      return error;
+    }
+  }
   _getLocationAsync = async () => {
     let { status } = await Permissions.askAsync(Permissions.LOCATION);
     if (status !== 'granted') {
@@ -76,7 +94,13 @@ export default class WayPoint extends Component {
         <MapView
           style={{ flex: 7 }}
           initialRegion={this.state.region}
-        />
+        >
+          <MapView.Polyline
+            coordinates={this.state.coords}
+            strokeWidth={2}
+            strokeColor="red"
+          />
+        </MapView>
         <View style={{ flex: 3}}>
           <Button
             disabled={this.state.disableButton}
