43ba355504dd7473276577604684f5c3baa5c241
Rob Aird <robkaird@gmail.com>
2017-11-30 15:48:07 -0600

    (feat) receives code/redirect from client

--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
+require('dotenv').config();
 const express = require('express');
+const OAuthServer = require('express-oauth-server');
+
+const { PORT, REACT_NATIVE_PACKAGER_HOSTNAME } = process.env;
 
 const app = express();
+app.OAuth = new OAuthServer({
+  model: {},
+});
 
 app.use(express.json());
 
-app.listen(8080, () => {
-  console.log('Connected!');
+app.post('/authorize', (req, res) => {
+  console.log('hello', req.body);
+  res.send(200);
+});
+
+app.listen({ host: REACT_NATIVE_PACKAGER_HOSTNAME, port: PORT }, () => {
+  // eslint-disable-next-line
+    console.log(`Listening on ${REACT_NATIVE_PACKAGER_HOSTNAME}:${PORT}`);
 });
