a5b0bb92c93d65d1c97813a2cccf27391142eff3
robertaird <robkaird@gmail.com>
2017-12-04 20:51:38 -0600

    (chore) dockerize

--- server/index.js
+++ server/index.js
@@ -18,21 +18,26 @@ app.get('/', (req, res) => {
 
 app.post('/authorize', async (req, res) => {
   const { code, redirectUrl } = req.body;
-  const data = await fbAuth.authorizeUser(code, redirectUrl);
+  console.log(req.body);
+  if (!code || !redirectUrl) {
+    res.send(400);
+  } else {
+    const data = await fbAuth.authorizeUser(code, redirectUrl);
 
-  if (data.error) {
-    res.sendStatus(500);
+    if (data.error) {
+      res.sendStatus(500);
+    }
+
+    const { access_token, expires_in } = data;
+    const user = await fbAuth.getUserProfile(access_token);
+    user.accessToken = { access_token, expires_in };
+    res.send(user);
   }
-
-  const { access_token, expires_in } = data;
-  const user = await fbAuth.getUserProfile(access_token);
-  user.accessToken = { access_token, expires_in };
-  res.send(user);
 });
 
 const serverParams = {
   port: PORT,
-}
+};
 
 if (REACT_NATIVE_PACKAGER_HOSTNAME) {
   serverParams.host = REACT_NATIVE_PACKAGER_HOSTNAME;
